3e80eb8ed0fa222f53e1cd6d0793749f40bfb4a6
skanehira <sho19921005@gmail.com>
2018-12-23 09:52:45 +0900

    add garden menu

--- front/src/components/garden/garden.vue
+++ front/src/components/garden/garden.vue
@@ -2,5 +2,21 @@
 
 <script>
 export default {
+    methods: {
+        toPage(page) {
+            this.$router.push("/"+ page);
+        },
+    }
 }
 </script>
+
+<style scoped>
+p {
+    text-align: center;
+}
+
+.menu {
+    animation: fadeIn 2s ease 0s 1 normal;
+    width: 200px;
+}
+</style>
