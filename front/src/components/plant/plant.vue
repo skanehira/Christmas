536af0f41af79f7bdc841c6dbc68ba6c504955c2
skanehira <sho19921005@gmail.com>
2018-12-24 20:11:17 +0900

    add plant

---
+++ front/src/components/plant/plant.vue
@@ -0,0 +1,21 @@
+<template src="./plant.html"></template>
+
+<script>
+export dafault {
+    data() {
+        return {
+            form: {
+                text: '',
+                category: '',
+                user_name: '',
+            },
+            categories: []
+        }
+    },
+        methods: {
+            regist: function() {
+
+            },
+        }
+}
+</script>
